6ac1845fa74f5198eb05b78324d0d27e4956c99e
Ram Peri <ramperi@google.com>
2023-05-23 14:53:28 -0400

    Merge branch 'upstream-google' into tm_sync
    Test: atest -c MyRoboTests
    Bug: 281899632
    Merged-In: Ic205da6e976c3c7cb1b91c332ddfd22d906ffe51
    Change-Id: Ic205da6e976c3c7cb1b91c332ddfd22d906ffe51

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDisplayManagerGlobal.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDisplayManagerGlobal.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import javax.annotation.Nullable;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.android.Bootstrap;
 import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
@@ -87,13 +86,26 @@ public class ShadowDisplayManagerGlobal {
             reflector(DisplayManagerGlobalReflector.class, instance);
     displayManagerGlobal.setDm(displayManager);
     displayManagerGlobal.setLock(new Object());
-    List<Handler> displayListeners =
-        RuntimeEnvironment.getApiLevel() < ShadowBuild.UPSIDE_DOWN_CAKE
-            ? new ArrayList<>()
-            : new CopyOnWriteArrayList<>();
+    List<Handler> displayListeners = createDisplayListeners();
     displayManagerGlobal.setDisplayListeners(displayListeners);
     displayManagerGlobal.setDisplayInfoCache(new SparseArray<>());
     return instance;
+  }
+
+  private static List<Handler> createDisplayListeners() {
+    try {
+      // The type for mDisplayListeners was changed from ArrayList to CopyOnWriteArrayList
+      // in some branches of T and U, so we need to reflect on DisplayManagerGlobal class
+      // to check the type of mDisplayListeners member before initializing appropriately.
+      Field f = DisplayManagerGlobal.class.getDeclaredField("mDisplayListeners");
+      if (f.getType().isAssignableFrom(ArrayList.class)) {
+        return new ArrayList<>();
+      } else {
+        return new CopyOnWriteArrayList<>();
+      }
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @VisibleForTesting
