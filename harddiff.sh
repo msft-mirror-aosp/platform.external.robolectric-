547e4497be68625477611bded60086aa6b9bb173
Rex Hoffman <rexhoffman@google.com>
2024-06-11 16:24:37 +0000

    Add two missing exclusions (native graphics related)
    Test: local execution
    Bug: None
    Change-Id: Ibcfaa66fb0e40b2bb03aef407278e1de16f6321b

--- harddiff.sh
+++ harddiff.sh
@@ -14,6 +14,7 @@ popd
 
 echo building patch
 # waiting on clean up the ttf/icu data -- hence nativeruntime resources are ignored.
+# we also need
 diff -Naur . /tmp/robo/robolectric \
   -x '*.bp' \
   -x '*.md' \
@@ -40,6 +41,8 @@ diff -Naur . /tmp/robo/robolectric \
   -x 'config.xml' \
   -x 'public.xml' \
   -x 'AndroidManifest.xml' \
+  -x 'README' \
+  -x 'robolectric.properties' \
 > /tmp/robo/patch
 
 echo applying patch
